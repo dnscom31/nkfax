7961d0cb451646fa455502bcae7691318d7f116d
dnscom31 <129644640+dnscom31@users.noreply.github.com>
2026-01-15 16:06:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,7 +180,7 @@ def create_fix_pdf(data):
 
         # 취소 사유
         if data['type'] == 'cancel':
-            add_text_to_image(draw, data['cancel_reason'], (300, 1300))
+            add_text_to_image(draw, data['cancel_reason'], (300, 1200))
 
         # [하단 날짜]
         today = datetime.now()
