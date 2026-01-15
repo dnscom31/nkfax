dd72eb9c283c4f2c58d02d921bc9634efb3697d9
dnscom31 <129644640+dnscom31@users.noreply.github.com>
2026-01-15 09:04:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,20 +86,20 @@ def create_report_pdf(data):
         # 2. 내용 입력(위치)
         target_date_str = data['checkup_date'].strftime("%Y년 %m월 %d일")
         #일시
-        add_text_to_image(draw, target_date_str, (320, 495))
+        add_text_to_image(draw, target_date_str, (320, 490))
 
         #시간
         time_str = f"{data['start_time'].strftime('%H:%M')} ~ {data['end_time'].strftime('%H:%M')}"
-        add_text_to_image(draw, time_str, (320, 525))
+        add_text_to_image(draw, time_str, (320, 530))
 
         #주소
-        add_text_to_image(draw, data['location'], (770, 495))
+        add_text_to_image(draw, data['location'], (730, 490))
 
         #업체명
         add_text_to_image(draw, data['target'], (320, 565))
 
         #인원
-        add_text_to_image(draw, f"{data['count']}명", (1000, 565))
+        add_text_to_image(draw, f"{data['count']}명", (950, 565))
 
         #의사명
         add_text_to_image(draw, data['doctor_name'], (620, 755))
@@ -108,11 +108,11 @@ def create_report_pdf(data):
         # 좌표는 배경 이미지의 '년', '월', '일' 글자 바로 앞부분으로 추정하여 잡았습니다.
         today = datetime.now()
         # 년 (900~930 근처)
-        add_text_to_image(draw, str(today.year), (830, 1100), font_size=18)
+        add_text_to_image(draw, str(today.year), (910, 1000), font_size=18)
         # 월 (1030 근처)
-        add_text_to_image(draw, str(today.month), (980, 1100), font_size=18)
+        add_text_to_image(draw, str(today.month), (980, 1000), font_size=18)
         # 일 (1120 근처)
-        add_text_to_image(draw, str(today.day), (1070, 1100), font_size=18)
+        add_text_to_image(draw, str(today.day), (1070, 1000), font_size=18)
 
         pdf_buffer = BytesIO()
         image.save(pdf_buffer, format="PDF", resolution=150.0)
